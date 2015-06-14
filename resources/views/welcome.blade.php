ce9b5c4dabe70c6dea03aeb9444cd862832717ee
Michael Norris <github@michaelnorris.co.uk>
2015-06-14 11:29:39 +0100

    add welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,23 +1,25 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+        <title>Tvrtle</title>
 
-        <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
-
+        <link href='http://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/animate.css/3.3.0/animate.min.css">
         <style>
             html, body {
                 height: 100%;
             }
 
             body {
+                background: #16a085;
                 margin: 0;
                 padding: 0;
                 width: 100%;
-                color: #B0BEC5;
+                color: #16a085;
                 display: table;
                 font-weight: 100;
-                font-family: 'Lato';
+                font-family: 'Indie Flower', cursive;
+                text-shadow: 0px 0px 1px #1abc9c, 0 0 2px #16a085, 0 0 5px #1abc9c;
             }
 
             .container {
@@ -32,21 +34,31 @@
             }
 
             .title {
-                font-size: 96px;
-                margin-bottom: 40px;
+                font-size: 192px;
+                margin-bottom: 48px;
             }
 
             .quote {
                 font-size: 24px;
             }
+
+            .title {
+                -webkit-animation-duration: 10s;
+            }
+
         </style>
     </head>
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
-                <div class="quote">{{ Inspiring::quote() }}</div>
+                <div class="title animated fadeIn">Tvrtle</div>
             </div>
         </div>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        <script>
+        $('.title').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+            $(this).addClass('pulse infinite').removeClass('fadeIn');
+        });
+        </script>
     </body>
 </html>
